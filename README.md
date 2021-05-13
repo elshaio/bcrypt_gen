d96689d5be8e621723706429e497727b94b91cea
Iván Román <develdkmd@outlook.com>
2021-05-13 09:47:48 -0600

    First commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Generating bcrypt passwords
+
+
+## Installation
+Please follow this instructions.
+
+```shell
+sudo apt install -y python3 python3-pip
+sudo python3 -m pip install -U pip virtualenv
+git clone 
+cd 
+virtualenv venv
+source venv/bin/activate
+pip install -r requirements.txt
+```
+
+## Use
+Just run this command
+```shell
+# Only once
+source venv/bin/activate
+python main.py [your password here]
+```
---
+++ main.py
@@ -0,0 +1,20 @@
+#!/home/concredito.com.mx/riromans/.virtualenvs/bcript/bin/python
+import bcrypt
+import sys
+import string
+
+
+def main(argumentos):
+    plainpassword = bytes(" ".join(argumentos), "utf8")
+    hashed = bcrypt.hashpw(plainpassword, bcrypt.gensalt(rounds=10, prefix=b"2a"))
+
+    if bcrypt.checkpw(plainpassword, hashed):
+        print("Al cien")
+        print("password: \n{}".format(plainpassword))
+        print("hashed: \n{}".format(hashed.decode()))
+    else:
+        print("oh-oh")
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    main(args)
